80754f6b920f705993a541c4611db8829fce1556
alvina-waqar <86957995+alvina-waqar@users.noreply.github.com>
2021-07-07 21:44:23 -0700

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,278 @@
+//alert("Error! please enter a valid password")
+
+//alert("welcome to JS world.." <br>  "Happy Coding!!" )
+
+//alert("welcome to JS world")
+//alert("Happy Coding")
+
+
+// var username
+// var myname = "alvina waqar"
+// alert(myname)
+
+// var js = "hello world"
+// var js1 = "alvina waqar"
+// var js2 = "15 years old"
+// var js3 = "certified mobile developement"
+// alert(js1)
+// alert(js2)
+// alert(js3)
+
+// var email = "my email address is example@example.com "
+// alert(email) 
+
+// var a = "yah! I can write HTML content through javascriptt"
+// document.write(a)
+
+// var a = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(a)
+
+// var a = "i am" + 15  + "years old"
+// alert(a)
+
+// var birthyear = "my birth year is" + 1998
+// document.write(birthyear)
+
+// var visitorname = prompt("enter your name")
+// var productname = prompt ("enter product tittle")
+// var quantity = prompt("enter quantity")
+// document.write(visitorname +"  odered  " + quantity + "- " + productname  + " on xyz clothing")
+
+//var name,fathername, age
+
+//var abc
+//var $abc
+//var _abc
+//var myname
+//var myname
+
+//var 12bd
+//var #abd
+//var 1name
+//var -hi
+
+
+//var heading = "Rules for naming JS variables"
+//document.write(" <br> ");
+//document.write(" <br> ");
+//document.write(heading)
+//var firstline = "Variable names can only contain $,letters, numbers and _."
+//document.write(" <br> ")
+//var  a = "Variables must begin with a $, _ and letters"
+//document.write(" <br> ");
+//var b = "Variable names are case sensitive"
+//document.write(" <br> ");
+//var c= "Variable names should not be JS keywords"
+//document.write(firstline)
+//document.write(a)
+//document.write(b)
+//document.write(c)
+
+//var num1 = 5
+//var num2 = 8
+//var sum = num1 + num2
+//document.write("sum of 5 and 8 is " + sum)
+
+
+//var num1 = 6
+//var num2 = 9
+//var sub = num2 - num1
+//document.write("the subtraction of 6 from 9 is "+ sub)
+
+//var num1 = 6
+//var num2 = 9
+//var multi = num2 * num1
+//document.write("the multiplication  of 6 and 9 is "+ multi)
+
+//var num1 = 9
+//var num2 = 3
+//var div = num1 / num2
+//document.write("the division of 9 and 3 is "+ div)
+
+//var num1 = 6
+//var num2 = 9
+//var mod = num2 % num1
+//document.write("the modulus of 6 and 9 is "+ mod)
+
+
+//var num1
+//document.write("Value after variable declaration is undefined")
+//num1 = 5
+//document.write("initial value is "+ num1)
+//num1++
+//document.write("value after increment is " + num1)
+//var add = num1 + 7
+//document.write("Value after addition is "+ add)
+//--add
+//document.write("value after decrement is "+ add)
+//var d = num1 % 3
+//document.write("The reminder is " + d)
+
+//var ct = 600
+//document.write("Total cost to buy 5 tickets to a movie is Rs." + ct*5)
+
+//var c = 65
+//var f = (65 * 9/5)+32
+//document.write("65'C is " + f +"'F")
+
+//var a =2
+//var b = 1
+//var c = --a - --b + ++b + b--
+//document.write(c)
+
+//var sub1 = prompt("enter 1st subject name")
+//var sub2 = prompt("enter 2nd subject name")
+//var sub3 = prompt("enter 3rd subject name")
+//var totalofeachsub = 100
+//var a = prompt("enter marks of 1st sub")
+//var b = prompt("enter marks of 2nd sub")
+//var c = prompt("enter marks of 3rd sub")
+//var total = a+b+c
+//var percent = total*100/300
+//document.write(sub1 + a)
+//document.write(sub2 + b)
+//document.write(sub3 + c)
+//document.write( total)
+//document.write(percent)
+
+//var city = karachi
+//var cityname = prompt("enter city name")
+//cityname = cityname.toLowerCase
+//if(cityname == 'karachi'){
+ // alert("welcome to city of lights")
+//}
+
+//var gender = prompt("Your Gender")
+
+// if (gender=="male"||gender=="Male") {
+//  alert("Good Morning Sir")
+// }
+// if("gender=="Female"||gender=="female"){
+//     alert("Good Morning  maam")
+// }
+
+// var color=prompt("input color of road traffic signal ")
+// if(color==="red"){
+//     alert("Must Stop")
+// }
+// if(color==="yellow")
+// {
+//     alert("Ready to move")
+// }
+// if(color==="green"){
+//     alert("Move now")
+// }
+
+
+//	var a = 4; if (++a === 5){ 
+  //  alert("given condition for variable a is true"); } 
+    
+//	var b = 82; if (b++ === 83){ 
+  //  alert("given condition for variable b is true"); 
+    //} 
+    
+//var c = 12; if (c++ === 13){ alert("condition 1 is true"); 
+//} if (c === 13){ alert("condition 2 is true"); 
+//} if (++c < 14){ alert("condition 3 is true"); 
+//} if(c === 14){ alert("condition 4 is true"); 
+//} 
+
+//var materialCost = 20000;
+//var laborCost = 2000; 
+//var totalCost = materialCost + laborCost; 
+//if (totalCost === laborCost + materialCost)
+  //  { alert("The cost equals"); }
+
+  //  if (true){ alert("True"); } if (false){ alert("False"); 
+//} 
+
+//if("car" < "cat"){ 
+  //  alert("car is smaller than cat"); 
+    //} 
+    
+//var secnum = 10
+//var guessno = prompt("guess secret no")
+//if(secnum === guessno ){
+  //  alert("BINGO! Correct Answer")
+//}
+//else(guessno == secnum+1)
+ //   alert('Close enough to the correct answer')
+
+
+ // var a=+prompt("Enter number")
+// if(a === 0 ){
+//     alert("The number is zero")
+// }
+// else if(a < 0 ){
+//     alert("The number is negative")
+// }
+// else if(a >0 ){
+//     alert("The number is positive")
+// }
+// else {
+//     alert("wrong input")
+// 
+
+//var literal = []
+//var object = Array("apple","banana")
+//var strings = ["pen","pencil","scale","eraser"]
+//var numbers = [5,7,8,9]
+//var boolean = [true,false]
+//var mixedarray = ["apple",67,true,"  ",prompt()]
+//console.log(mixedarray)
+//var edu = ["ssc","hsc",'bsc','b.com','masters','mphil','phd']
+
+//var colors = ['red','yellow','blue','purple','black']
+//document.write(colors + "<br>")
+//colors.unshift(prompt("enter color name which you want to add in beginning"))
+//document.write(colors + '<br>')
+//colors.push(prompt("enter color name which you want to add in the end"))
+//document.write(colors + '<br>')
+//colors.splice(0,0,prompt('add colours name'),prompt('add color name'))
+//document.write(colors+ '<br>')
+//colors.shift()
+//document.write(colors + '<br>')
+//colors.pop()
+//document.write(colors)
+//colors.splice(prompt('at what index you want to add new color name'),0,prompt('enter color name'))
+//document.write(colors+ '<br>')
+//colors.splice(prompt('from which index you want to delete'),prompt('how many colors you want to delete'))
+//document.write(colors)
+
+//var scores = ['300','120','459','250']
+//document.write('Scores of students'+scores + '<br>')
+//scores.sort()
+//document.write('ordered scores of students'+scores)
+
+//var citynames = ['islamabad','karachi','quetta','lahore','murree']
+//document.write('Cities list'+ '<br>' + citynames + '<br>')
+//var seccitynames =citynames.slice(0,3)
+//document.write('slected cities list'+'<br>' + seccitynames)
+
+//var arr = ['this','is','my','car']
+///document.write('Array' + arr + '<br>')
+//var n = arr.join()
+//document.write('string'+ n)
+
+//var arr = [1,2,3,4,5]
+//for(i=0 ; i<=arr.length ; i++){
+ // for(j=0 ; j<arr.length ; j++ ){
+   // document.write(arr)  
+ // }
+//}
+
+//const number = parseInt(prompt('Enter an integer: '));
+
+//for(let i = 1; i <= 10; i++) {
+  //  const result = i * number;
+    //console.log(`${number} * ${i} = ${result}`)}
+
+//var fruits = ['apple','banana','grapes','orange']
+//for(i=0; i<fruits.length ; i++){
+ // document.write('At index:'+ [i] + ' ' + fruits[i]  +'<br>')
+//}
+
+
+
+
+
